7398b38470bdf5269fc0f538aee93442264fe274
U-LadyLightning\Raphael <Raphael@LadyLightning.(none)>
2014-02-15 22:16:08 -0800

    asdf

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+
+activate :directory_indexes
+
 ###
 # Compass
 ###
@@ -51,6 +54,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :relative_links, true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -63,7 +68,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
